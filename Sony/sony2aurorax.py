7ae55f944480cc2501020e8252b03aac206206d8
mikkosyrjasuo <60669066+mikkosyrjasuo@users.noreply.github.com>
2024-09-16 09:39:10 +0200

    Update image resizing

--- Sony/sony2aurorax.py
+++ Sony/sony2aurorax.py
@@ -28,13 +28,9 @@ import os
 import re
 import datetime as dt
 from PIL import Image, ImageDraw, ImageFont
+import cv2
 
-
-def oneDayThumbnails(year,month,day):
-    #keoname=f'LYR-KHO-{year}{month:02}{day:02}.jpg'
-#    if os.path.isfile(keoname)==True:
-#        print(f'Keogram {keoname} exists, skipping...')
-#        return
+def oneDayThumbnails(year,month,day,overWriteExisting=False):
     basepath=os.path.join("D:\\","KHO","Sony",f'{year:04}', f'{month:02}',
                           f'{day:02}','Images')
     imagefiles=glob.glob(os.path.join(basepath,"*.jpg"))
@@ -88,56 +84,51 @@ def oneDayThumbnails(year,month,day):
         auroraXdir=os.path.join(auroraXpath,f'{fileyear}',f'{filemonth:02}',f'{fileday:02}')
         auroraXname=f'LYR-Sony-{fileyear}{filemonth:02}{fileday:02}_{filehh:02}{filemm:02}{filess:02}.jpg'
         auroraXthumb=os.path.join(auroraXdir,auroraXname)
-        if os.path.isfile(auroraXthumb)==True:
-            print(f'    {auroraXname} exists, skipping...')
-            continue
+
+        if overWriteExisting==False:
+            if os.path.isfile(auroraXthumb)==True:
+                print(f'    {auroraXname} exists, skipping...')
+                continue
         try:
             os.makedirs(auroraXdir) # Create possibly missing subdirectories
         except FileExistsError:
             pass
 
-        with Image.open(imagefiles[i]).resize((480,480),resample=Image.Resampling.NEAREST) as im:
-            font1=ImageFont.truetype(r"C:\Windows\Fonts\arialbd.ttf",20)
-            font2=ImageFont.truetype(r"C:\Windows\Fonts\courbd.ttf",16)
-            d=ImageDraw.Draw(im)
-            d.text((5,3),"UNIS/KHO", font=font1, fill=(255,255,255))
-            d.text((5,30),"Sony A7s", font=font1, fill=(255,255,255))
-            d.text((5,460),f'{fileyear}-{filemonth:02}-{fileday:02}', font=font2, fill=(255,255,255))
-            d.text((365,460),f'{filehh:02}:{filemm:02}:{filess:02} UT', font=font2, fill=(255,255,255))
-            d.text((440,3)," N ", font=font2, fill=(255,255,255))
-            d.text((440,3+16),"E W ", font=font2, fill=(255,255,255))
-            d.text((440,3+16+16)," S ", font=font2, fill=(255,255,255))
-            im.save(auroraXthumb,"JPEG", quality=85, optimize=True)
-            print(f'Created {auroraXname}')
+        # Our scientists preferred the CV2 library to PIL for resizing the images...
+        image=cv2.imread(imagefiles[i])
+        w=480
+        h=480
+        resized_image=cv2.resize(image,(w,h),interpolation=cv2.INTER_NEAREST)
+        #resized_image=cv2.resize(image,(w,h),interpolation=cv2.INTER_LANCZOS4)
+
+        # Add captions: the text writing routines in PIL are much more
+        # flexible...
+        cv2_im=cv2.cvtColor(resized_image,cv2.COLOR_BGR2RGB)
+        im=Image.fromarray(cv2_im)
         
+        font1=ImageFont.truetype(r"C:\Windows\Fonts\arialbd.ttf",20)
+        font2=ImageFont.truetype(r"C:\Windows\Fonts\courbd.ttf",16)
+        d=ImageDraw.Draw(im)
+        d.text((5,3),"UNIS/KHO", font=font1, fill=(255,255,255))
+        d.text((5,30),"Sony A7s", font=font1, fill=(255,255,255))
+        d.text((5,460),f'{fileyear}-{filemonth:02}-{fileday:02}', font=font2, fill=(255,255,255))
+        d.text((365,460),f'{filehh:02}:{filemm:02}:{filess:02} UT', font=font2, fill=(255,255,255))
+        d.text((440,3)," N ", font=font2, fill=(255,255,255))
+        d.text((440,3+16),"E W ", font=font2, fill=(255,255,255))
+        d.text((440,3+16+16)," S ", font=font2, fill=(255,255,255))
+        im.save(auroraXthumb,"JPEG", quality=85, optimize=True)
+        print(f'Created {auroraXthumb}')
+
+        with open(os.path.join(auroraXpath,"latest_thumbnail.txt"),'w') as f:
+            f.write(f'{auroraXthumb}')
 
 
 #=====================================================================
 # Manual keograms before archiving to NIRD...
 
-# year=2023
-# for month in (11,12):
-#     for day in np.arange(1,32):
-#         oneDayThumbnails(year,month,day)
-
-# year=2024
-# for month in (1,2,3):
-#     for day in np.arange(1,32):
-#         oneDayThumbnails(year,month,day)
-
-# year=2019
-# for month in (1,2,11,12):
-#     for day in np.arange(1,32):
-#         oneDayThumbnails(year,month,day)
-
-year=2018
-for month in np.arange(2,13):
-    for day in np.arange(1,32):
-        oneDayThumbnails(year,month,day)
-
-# years=(2018,2019,2020,2021,2022,2023,2024)
-# for year in years:
-#     for month in (1,2,3,4,10,11,12):
-#         for day in np.arange(1,32):
-#             oneDayThumbnails(year,month,day)
+years=(2016,2017,2018,2019,2020,2021,2022,2023,2024)
+for year in years:
+    for month in (1,2,3,4,10,11,12):
+        for day in np.arange(1,32):
+            oneDayThumbnails(year,month,day,overWriteExisting=True)
 
