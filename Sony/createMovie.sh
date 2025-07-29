71d6a91f8fc3406b876047ed6efe0ddaaf904c4e
mikkosyrjasuo <60669066+mikkosyrjasuo@users.noreply.github.com>
2025-07-29 11:01:38 +0200

    Update createMovie.sh

--- Sony/createMovie.sh
+++ Sony/createMovie.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+if [ "$#" -ne 1 ]; then
+    echo "createMovie2pass {outputfile}"
+    exit 0
+fi 
+
 
 # The mp4-file naming convention is strict and should be YYYYMMDD_unis_kho_sony-rgb.mp4
-
-ffmpeg -f image2 -framerate 10 -start_number 0 -i Frames/frame%04d.jpg -vframes 7200 -b:v 300k -c:v libx264 20241201_unis_kho_sony-rgb.mp4
+# NOTE: the number of frames needs has wwo possibilities: 7200 (12-sec cadence) or 8640 (10-sec cadence)
+ffmpeg -f image2 -framerate 10 -start_number 0 -i Frames/frame%04d.jpg -vframes 7200 -c:v libx264 $1
