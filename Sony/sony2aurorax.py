059570544578ffa49bb5a20c425b7f4022cf0592
Mikko Syrjäsuo <60669066+mikkosyrjasuo@users.noreply.github.com>
2025-11-28 11:25:27 +0100

    Update image interpolation

--- Sony/sony2aurorax.py
+++ Sony/sony2aurorax.py
@@ -7,21 +7,50 @@ any missing "thumbnail" images suitable for AuroraX (480x480 jpg)
 @author: MikkoS
 """
 
+"""
+Finding which fonts are available in this system:
+    
+matplotlib.font_manager.findSystemFonts(fontpaths=None, fontext='ttf') 
+
+Arial Bold
+
+font=ImageFont.truetype(r"C:\Windows\Fonts\arialbd.ttf",20)
+
+Courier
+
+font=ImageFont.truetype(r"C:\Windows\Fonts\couri.ttf",14)
+
+"""
+
 import glob
 import numpy as np
 import os
 import re
 import datetime as dt
 from PIL import Image, ImageDraw, ImageFont
-import cv2
+#import cv2
 
 def oneDayThumbnails(year,month,day,overWriteExisting=False):
-    basepath=os.path.join('/','home','mikkos','Data',f'{year:04}', f'{month:02}',
-                          f'{day:02}')
-    imagefiles=glob.glob(os.path.join(basepath,'*.jpg'))
-    imagefiles.sort()
+    #keoname=f'LYR-KHO-{year}{month:02}{day:02}.jpg'
+#    if os.path.isfile(keoname)==True:
+#        print(f'Keogram {keoname} exists, skipping...')
+#        return
 
-    auroraXpath=os.path.join('/','home','mikkos','Data','Quicklooks')
+    basepath=os.path.join("D:\\","KHO","Sony",f'{year:04}', f'{month:02}',
+                          f'{day:02}','Images')
+
+#    basepath=os.path.join("E:\\","Move2NIRD",f'{year:04}', f'{month:02}',
+#                          f'{day:02}','Images')
+
+    print(basepath)
+    imagefiles=glob.glob(os.path.join(basepath,"*.jpg"))
+    imagefiles.sort()
+    
+
+    auroraXpath=os.path.join("D:\\","KHO","Sony","AuroraX", "Quicklooks")
+    #if os.path.exists(auroraXpath):
+    #    print(f'Data exists, skipping {auroraXpath}')
+    #    return
     
     # The Sony A7S filename convention is fixed to
     # LYR-Sony-DDMMYY_HHMMSS.jpg, so, for example,
@@ -38,18 +67,19 @@ def oneDayThumbnails(year,month,day,overWriteExisting=False):
         # First check whether the name is of correct format and then check
         # for a valid date and time before continuing with the keogram
 
-        filepattern=r"LYR-Sony-(\d\d\d\d)(\d\d)(\d\d)_(\d\d)(\d\d)(\d\d).jpg"
+        filepattern=r"LYR-Sony-(\d\d)(\d\d)(\d\d)_(\d\d)(\d\d)(\d\d).jpg"
         checkname=re.match(filepattern, thisfile)
-        if checkname == None:
+        if checkname == False:
             continue
 
         # There is probably a more stylish way to do this in python
         # but at least this is easy to understand...
+
         validname=re.split(filepattern, thisfile)
 
-        fileday=int(validname[3])
+        fileday=int(validname[1])
         filemonth=int(validname[2])
-        fileyear=int(validname[1])
+        fileyear=2000+int(validname[3])
 
         filehh=int(validname[4])
         filemm=int(validname[5])
@@ -63,7 +93,6 @@ def oneDayThumbnails(year,month,day,overWriteExisting=False):
         except ValueError:
             print('Funny date and time in',thisfile)
             continue
-
 
         # Create a new directory if needed
         auroraXdir=os.path.join(auroraXpath,f'{fileyear}',f'{filemonth:02}',f'{fileday:02}')
@@ -79,46 +108,52 @@ def oneDayThumbnails(year,month,day,overWriteExisting=False):
         except FileExistsError:
             pass
 
-        # Our scientists preferred the CV2 library to PIL for resizing the images...
-        image=cv2.imread(imagefiles[i])
-        w=480
-        h=480
-        resized_image=cv2.resize(image,(w,h),interpolation=cv2.INTER_NEAREST)
-        #resized_image=cv2.resize(image,(w,h),interpolation=cv2.INTER_LANCZOS4)
+#        image=cv2.imread(imagefiles[i])
+#        w=480
+#        h=480
+        #resized_image=cv2.resize(image,(w,h),interpolation=cv2.INTER_NEAREST)
+#        resized_image=cv2.resize(image,(w,h),interpolation=cv2.INTER_LANCZOS4)
 
         # Add captions: the text writing routines in PIL are much more
         # flexible...
-        cv2_im=cv2.cvtColor(resized_image,cv2.COLOR_BGR2RGB)
-        im=Image.fromarray(cv2_im)
-
-        font1base=r"/usr/share/fonts/opentype/noto" 
-        font2base=r"/usr/share/fonts/truetype/ubuntu"
-        font1=ImageFont.truetype(os.path.join(font1base,"NotoSansCJK-Bold.ttc"),20)
-        font2=ImageFont.truetype(os.path.join(font2base,"UbuntuMono-R.ttf"),18)
-        d=ImageDraw.Draw(im)
-        d.text((5,3),"UNIS/KHO", font=font1, fill=(255,255,255))
-        d.text((5,30),"Sony A7s", font=font1, fill=(255,255,255))
-        d.text((5,460),f'{fileyear}-{filemonth:02}-{fileday:02}', font=font2, fill=(255,255,255))
-        d.text((365,460),f'{filehh:02}:{filemm:02}:{filess:02} UT', font=font2, fill=(255,255,255))
-        d.text((440,3)," N ", font=font2, fill=(255,255,255))
-        d.text((440,3+16),"E W", font=font2, fill=(255,255,255))
-        d.text((440,3+16+16)," S ", font=font2, fill=(255,255,255))
-        im.save(auroraXthumb,"JPEG", quality=85, optimize=True)
-        print(f'Created {auroraXthumb}')
-#        with open(os.path.join(auroraXpath,"latest_thumbnail.txt"),'w') as f:
-#            f.write(f'{auroraXthumb}')
+#        cv2_im=cv2.cvtColor(resized_image,cv2.COLOR_BGR2RGB)
+#        im=Image.fromarray(cv2_im)
+        
+        with Image.open(imagefiles[i]).resize((480,480)) as im:
+            font1=ImageFont.truetype(r"C:\Windows\Fonts\arialbd.ttf",20)
+            font2=ImageFont.truetype(r"C:\Windows\Fonts\courbd.ttf",16)
+            d=ImageDraw.Draw(im)
+            d.text((5,3),"UNIS/KHO", font=font1, fill=(255,255,255))
+            d.text((5,30),"Sony A7s", font=font1, fill=(255,255,255))
+            d.text((5,460),f'{fileyear}-{filemonth:02}-{fileday:02}', font=font2, fill=(255,255,255))
+            d.text((365,460),f'{filehh:02}:{filemm:02}:{filess:02} UT', font=font2, fill=(255,255,255))
+            d.text((440,3)," N ", font=font2, fill=(255,255,255))
+            d.text((440,3+16),"E W ", font=font2, fill=(255,255,255))
+            d.text((440,3+16+16)," S ", font=font2, fill=(255,255,255))
+            im.save(auroraXthumb,"JPEG", quality=85, optimize=True)
+            print(f'Created {auroraXthumb}')
+        
+            with open(os.path.join(auroraXpath,"latest_thumbnail.txt"),'w') as f:
+                f.write(f'{auroraXthumb}')
+        
 
 
 #=====================================================================
 # Manual keograms before archiving to NIRD...
 
-for day in np.arange(1,32):
-    oneDayThumbnails(2024,10,day)
+#oneDayThumbnails(2022,2,4)
 
+years=(2017,) #2018,2019,2020)
+months=(1,) #2,11,12) #,11,12)
+for year in years:
+    for month in months:
+        for day in np.arange(1,2):
+            oneDayThumbnails(year, month, day,overWriteExisting=False)
 
 #years=(2016,2017,2018,2019,2020,2021,2022,2023,2024)
+#months=(1,2,11,12)
 #for year in years:
-#    for month in (1,2,3,4,10,11,12):
+#    for month in months:
 #        for day in np.arange(1,32):
-#            oneDayThumbnails(year,month,day,overWriteExisting=True)
+#            oneDayThumbnails(year,month,day,overWriteExisting=False)
 
